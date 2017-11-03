bf227bfb53c35db78014d57a0e039f0802be72b1
franguyarmani <fbpeabody@gmail.com>
2017-11-02 22:39:57 -0400

    added function to remove stage directions in brackets

--- CleaningFunctions.py
+++ CleaningFunctions.py
@@ -1,4 +1,4 @@
-
+import string
 def remove_all_caps(dirtyList):
     cleanList = []
     for word in dirtyList:
@@ -12,3 +12,13 @@ def remove_punctandnums(dirtyList):
         if word not in string.punctuation:
             cleanList.append(word)
     return cleanList
+
+def remove_directions_brackets(rawText):
+    cleanString = rawText
+    while cleanString.find('[') != -1:
+        start = cleanString.find('[')
+        end = cleanString.find(']')
+        front = cleanString[:start-1]
+        back = cleanString[end+1:]
+        cleanString = front+back
+    return cleanString
