ba1fca3ba2811d76ac214906cfdbdfba90655d99
franguyarmani <fbpeabody@gmail.com>
2017-11-05 21:49:21 -0500

    added helperfuncitons module

---
+++ HelperFunctions.py
@@ -0,0 +1,14 @@
+from nltk.tokenize import WordPunctTokenizer
+from nltk.stem.wordnet import WordNetLemmatizer 
+import CleaningFunctions as c
+
+
+def cleaner(rawText):
+    noBrackets = c.remove_directions_brackets(rawText)
+    noDirections = c.remove_directions_space(noBrackets)
+    noNames = c.remove_abrv_names(noDirections)
+    tokenized = WordPunctTokenizer().tokenize(noNames)
+    noCaps = c.remove_all_caps(tokenized)
+    noPunct = c.remove_punctandnums(noCaps)
+    lemmatized = WordNetLemmatizer().lemmatize(noPunct)
+    return cleanList
