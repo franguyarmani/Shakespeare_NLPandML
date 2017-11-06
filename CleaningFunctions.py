0f29664b7e414335f2ff2fcbbce65c953f14b315
franguyarmani <fbpeabody@gmail.com>
2017-11-05 21:43:26 -0500

    add abreviated names cleaning function

--- CleaningFunctions.py
+++ CleaningFunctions.py
@@ -1,4 +1,44 @@
 import string
+import re
+
+#---------------Raw Text Cleaners---------------
+def remove_directions_brackets(rawText): #Safe to use on all plays
+    cleanString = rawText
+    while cleanString.find('[') != -1:
+        start = cleanString.find('[')
+        end = cleanString.find(']')
+        cleanString = splice(start, end, cleanString)
+        
+    return cleanString
+
+def remove_directions_space(rawText): #tested for as you like it, Caesar, 
+    #use a regular expresison to find space before direction and return after
+    cleanString = rawText
+    dir1 = re.compile('\S {8}.+\n| {20}.+\n')
+    match = dir1.search(cleanString)
+    directions = []
+    while match != None:
+        start = match.start()
+        end = match.end()
+        directions.append(cleanString[start:end])
+        cleanString = splice(start, end, cleanString)
+        match = dir1.search(cleanString)
+    return cleanString
+
+def remove_abrv_names(rawText): #Safe to run on Hamlet, King Lear, MacBeth, Much Ado, Othello,
+    cleanString = rawText
+    test = re.compile('\n *[A-Z][a-z]{2}\.|\n *[A-Z][a-z]{3}\.|\n *[A-Z][a-z]{4}\.')
+    match = test.search(cleanString)
+    cuts = []
+    while match != None:
+        start = match.start()
+        end = match.end()
+        cuts.append(cleanString[start:end])
+        cleanString = splice(start, end, cleanString)
+        match = test.search(cleanString)
+    return cleanString
+
+#--------------List Cleaners--------------
 def remove_all_caps(dirtyList):
     cleanList = []
     for word in dirtyList:
@@ -13,12 +53,15 @@ def remove_punctandnums(dirtyList):
             cleanList.append(word)
     return cleanList
 
-def remove_directions_brackets(rawText):
-    cleanString = rawText
-    while cleanString.find('[') != -1:
-        start = cleanString.find('[')
-        end = cleanString.find(']')
-        front = cleanString[:start-1]
-        back = cleanString[end+1:]
-        cleanString = front+back
-    return cleanString
+
+
+
+
+
+
+#++++++++++Helper Funcitons++++++++++++
+def splice(index1, index2, string):
+    front = string[:index1-1]
+    back = string[index2+1:]
+    newString = front+back
+    return newString
