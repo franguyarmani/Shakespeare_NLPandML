ea8bc84c12adf669894440fed1ab4ed91351037a
franguyarmani <fbpeabody@gmail.com>
2017-11-07 18:37:02 -0500

    added functionality to extract the 'to be' soliloque and the speech form the harry potter excert

--- Part2/main.py
+++ Part2/main.py
@@ -3,6 +3,8 @@ from nltk.tokenize import WordPunctTokenizer
 
 import re
 
+import codecs
+
 
 #===================The Corpi======================
 shakespeareRoot = "C:/Users/fbpea/Git_Repositories/Shakespeare_NLPandML/ShakespeareCorpus"
@@ -16,10 +18,25 @@ HP = PlaintextCorpusReader(harryPotterRoot, 'HarryPotterExcerpt.txt')
 HamletRaw = plays.raw('Hamlet.txt')
 DumbledoreRaw = HP.raw('HarryPotterExcerpt.txt')
 #=================Extract Content===================
-re.DOTALL
 soliloquyStart = re.compile('To be, or not to be')
 soliloquyEnd = re.compile('Be all my sins remember\'d\.')
 Start = soliloquyStart.search(HamletRaw)
 End = soliloquyEnd.search(HamletRaw)
+soliloquy = HamletRaw[Start.start():End.end()]
 
-print(HamletRaw[Start.start():End.end()])
+
+quotelist = DumbledoreRaw.split('\"')[1::2] #creates a list of words split on quotation marks. 
+                                            #Then selects the odd elements, which are the words between the quotes only. 
+speech = " ".join(quotelist)                #joins the quoted text
+
+#==================Count Sentences===================
+sentEnders = re.compile('[.,?,:]')
+soliloquySents = len(re.split('[.,?,:]', soliloquy))
+speechSents = len(speech.split('.'))
+print(soliloquySents)
+
+
+
+
+
+#print(HamletRaw[Start.start():End.end()])
