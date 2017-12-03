b7c60d9e96257e4219291781aeba69e863a86100
franguyarmani <fbpeabody@gmail.com>
2017-12-03 17:06:37 -0500

    completed bag of words representation

---
+++ MachineLearning/BOW.py
@@ -0,0 +1,40 @@
+import os
+import string
+import Utilities as u
+
+arff = open("BagOfWord.arff", 'w')
+scenesPath = "C:/Users/fbpea/Git_Repositories/Shakespeare_NLPandML/Scenes"
+
+os.chdir(scenesPath)
+
+def build_vocabulary(Path):
+    for filename in os.listdir(scenesPath):
+        for w in u.prepare_text(filename):
+            words[w] = 1
+
+words = {}
+build_vocabulary(scenesPath)
+vocab = list(words.keys())
+arff.write("@RELATION scenes\n")
+for word in vocab:
+    arff.write("@Attribute " + word + " REAL\n" )
+arff.write("@ATTRIBUTE Tragedy {Tragedy, Comedy}\n")
+arff.write("@DATA\n")
+for filename in os.listdir(scenesPath):
+    sceneWords = u.prepare_text(filename)
+    vector = u.build_vector(sceneWords, list(words.keys()))
+    if(filename[0] == "t"):
+        for word in vocab:
+            arff.write(str(vector[word])+",")
+        arff.write("Tragedy\n")
+    else:
+        for word in vocab:
+            arff.write(str(vector[word])+",")
+        arff.write("Comedy\n")
+arff.close()    
+
+
+
+
+
+
