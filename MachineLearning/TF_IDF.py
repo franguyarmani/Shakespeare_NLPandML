78c595e361b70bc0e500683fff6c74438909912e
franguyarmani <fbpeabody@gmail.com>
2017-12-03 18:43:08 -0500

    finished TF-IDF model

--- MachineLearning/TF_IDF.py
+++ MachineLearning/TF_IDF.py
@@ -1,5 +1,7 @@
+import math
 import os
 import string
+
 import Utilities as u
 
 arff = open("TF_IDF.arff", 'w')
@@ -7,35 +9,47 @@ scenesPath = "C:/Users/fbpea/Git_Repositories/Shakespeare_NLPandML/Scenes"
 
 os.chdir(scenesPath)
 
-def build_vocabulary(Path):
-    d = {}
-    for filename in os.listdir(scenesPath):
-        for w in u.prepare_text(filename):
-            d[w] = 1
+def build_occurences(vocabulary, Path):
+    d = dict.fromkeys(vocabulary, 0)
+    for filename in os.listdir(Path):
+        text = u.prepare_text(filename)
+        for w in vocab:
+            if (w in text):
+                d[w] += 1
+    return d    
+
+def build_weights(vocabulary, ocr):
+    d = dict.fromkeys(vocabulary, 0)
+    for w in vocabulary:
+        d[w] = math.log10(nDocs/ocr[w])
     return d
+
+
+
 
 words = u.build_vocabulary(scenesPath)
 vocab = list(words.keys())
+occurences = build_occurences(vocab, scenesPath)
+nDocs = len(os.listdir(scenesPath))
+weights = build_weights(vocab, occurences)
+
+
 arff.write("@RELATION scenes\n")
 for word in vocab:
-    arff.write("@Attribute " + word + " REAL\n" )
+    arff.write("@Attribute " + word + " REAL\n")
 arff.write("@ATTRIBUTE Tragedy {Tragedy, Comedy}\n")
 arff.write("@DATA\n")
 for filename in os.listdir(scenesPath):
     sceneWords = u.prepare_text(filename)
     vector = u.build_vector(sceneWords, list(words.keys()))
+
     if(filename[0] == "t"):
         for word in vocab:
-            arff.write(str(vector[word])+",")
+            arff.write(str(vector[word]*weights[word])+",")
+
         arff.write("Tragedy\n")
     else:
         for word in vocab:
-            arff.write(str(vector[word])+",")
+            arff.write(str(vector[word]*weights[word])+",")
         arff.write("Comedy\n")
 arff.close()    
-
-
-
-
-
-
